2886dad1b9a4c33fb8d8e08639939362bfe27c73
Aluno <e023aluno@outlook.com>
2026-05-21 21:49:34 -0300

    Finalização da tela de Contato

--- frontend/src/components/pages/Contato/contato.jsx
+++ frontend/src/components/pages/Contato/contato.jsx
@@ -1,7 +1,35 @@
-export default function Contato(){
-    return(
-        <div>
-            <h1>Teste</h1>
+import "./contato.css";
+import { AiFillMail } from "react-icons/ai";
+
+export default function Contato() {
+  return (
+    <div className="contato-pagina">
+      <div className="contato-esquerda">
+        <h1>Contate-nos</h1>
+
+        <div className="info">
+          <p>Caso queira nos enviar uma sugestão!</p>
+          <br />
+          <p><strong>Email:</strong></p>    
+          <p className="email-linha"> 
+            <AiFillMail className="icon-contato"/> onedevsofficial@gmail.com
+          </p>
         </div>
-    )
+      </div>
+
+      <div className="contato-direita">
+        <div className="card-branco">
+          <h1>Envie-nos uma mensagem</h1>
+
+          <form>
+            <input type="text" placeholder="Nome" />
+            <input type="email" placeholder="Email" />
+            <textarea placeholder="Mensagem"></textarea>
+
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
