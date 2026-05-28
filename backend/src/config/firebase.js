2e67942907702c52c5e1e0d4b20d61cd0d572585
Aluno <e023aluno@outlook.com>
2026-05-28 20:08:28 -0300

    chore: correção do backend para ligar servidor no render.

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,11 +1,9 @@
 import admin from "firebase-admin"
 
+import serviceAccount from "/etc/secrets/onedevsos-66dcd-firebase-adminsdk-fbsvc-2348ae4cdc.json" with { type: 'json'}
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY
-})
+  credential: admin.credential.cert(serviceAccount)
 
 });
 
