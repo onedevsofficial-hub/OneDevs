087451fdf50d276c4a4493e3df6e3936b646963a
Aluno <e023aluno@outlook.com>
2026-05-28 20:29:08 -0300

    chore: Atualização do server.js para incluir porta no render

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import router from './src/route/newsletterRoute.js';
 import cadastrarEmailRouter from './src/route/cadastrarEmailRoute.js';
 
+const port = process.env.PORT || 3000
+
 const app = express();
 app.use(cors())
 app.use(express.json())
@@ -10,6 +12,6 @@ app.use(express.json())
 app.use(router)
 app.use(cadastrarEmailRouter)
 
-app.listen(3000, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
     console.log("servidor ligado")
 })
